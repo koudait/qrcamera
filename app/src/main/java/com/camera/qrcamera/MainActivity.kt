ae5bc302f003af2e968476d03dc54dbf495158ac
keisukemukaida <keisukemukaida>
2022-03-22 12:23:01 +0900

    test

--- app/src/main/java/com/camera/qrcamera/MainActivity.kt
+++ app/src/main/java/com/camera/qrcamera/MainActivity.kt
@@ -1,30 +1,32 @@
 package com.camera.qrcamera
 import android.Manifest
 import android.content.pm.PackageManager
-import android.graphics.Matrix
 import android.os.Bundle
 import android.util.Size
-import android.view.Surface
-import android.view.TextureView
-import androidx.camera.lifecycle.ProcessCameraProvider
 import android.widget.Toast
+import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.appcompat.app.AppCompatActivity
 import androidx.camera.core.*
 import androidx.camera.view.PreviewView
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.LifecycleOwner
+import com.google.common.util.concurrent.ListenableFuture
+import java.lang.Exception
 import java.util.concurrent.Executors
 
 private const val REQUEST_CODE_PERMISSIONS = 10
 private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA,Manifest.permission.WRITE_EXTERNAL_STORAGE)
 
 class MainActivity : AppCompatActivity() {
+    //後に利用するTextureViewをここで定義します。
+    private lateinit var cameraProviderFuture : ListenableFuture<ProcessCameraProvider>
+    private lateinit var previewView: PreviewView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        previewView = findViewById<PreviewView>(R.id.previewView)
+        previewView = findViewById(R.id.previewView)
 
         if (allPermissionsGranted()) {
             startCamera()
@@ -33,28 +35,27 @@ class MainActivity : AppCompatActivity() {
                 this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
         }
     }
-    //カメラ撮影時に利用する単一のスレッドを取得し再利用できるようにexecutorを作成します。
-    private val executor = Executors.newSingleThreadExecutor()
-
-    //後に利用するTextureViewをここで定義します。
-    private lateinit var previewView: PreviewView
 
     private fun startCamera() {
-        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
-        cameraProviderFuture.addListener(Runnable {
-            val cameraProvider = cameraProviderFuture.get()
-            var preview : Preview = Preview.Builder()
-                .setTargetResolution(Size(640, 480))
-                .build()
-            var cameraSelector : CameraSelector = CameraSelector.Builder()
-                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
-                .build()
+        try {
+            cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+            cameraProviderFuture.addListener(Runnable {
+                val cameraProvider = cameraProviderFuture.get()
+                var preview : Preview = Preview.Builder()
+                    .setTargetResolution(Size(640, 480))
+                    .build()
+                var cameraSelector : CameraSelector = CameraSelector.Builder()
+                    .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                    .build()
 
-            preview.setSurfaceProvider(previewView.surfaceProvider)
+                preview.setSurfaceProvider(previewView.surfaceProvider)
 
-            var camera = cameraProvider.bindToLifecycle(this as LifecycleOwner, cameraSelector, preview)
+                var camera = cameraProvider.bindToLifecycle(this as LifecycleOwner, cameraSelector, preview)
 
-        }, ContextCompat.getMainExecutor(this))
+            }, ContextCompat.getMainExecutor(this))
+        } catch (e:Exception) {
+            Toast.makeText(this, "Camera Error", Toast.LENGTH_LONG)
+        }
     }
 
     //マニフェストで指定されたすべての権限が付与されているかどうかを確認します
