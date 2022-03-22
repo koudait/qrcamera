e5ff553236a3903857b9ddd48e99ef19c8427e98
keisukemukaida <keisukemukaida>
2022-03-22 11:59:25 +0900

    test

--- app/src/main/java/com/camera/qrcamera/MainActivity.kt
+++ app/src/main/java/com/camera/qrcamera/MainActivity.kt
@@ -3,21 +3,17 @@ import android.Manifest
 import android.content.pm.PackageManager
 import android.graphics.Matrix
 import android.os.Bundle
-import android.util.Log
 import android.util.Size
 import android.view.Surface
 import android.view.TextureView
-import android.view.ViewGroup
-import android.widget.ImageButton
+import androidx.camera.lifecycle.ProcessCameraProvider
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.camera.core.*
-import androidx.camera.core.impl.ImageCaptureConfig
-import androidx.camera.core.impl.PreviewConfig
-
+import androidx.camera.view.PreviewView
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import java.io.File
+import androidx.lifecycle.LifecycleOwner
 import java.util.concurrent.Executors
 
 private const val REQUEST_CODE_PERMISSIONS = 10
@@ -28,10 +24,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        viewFinder = findViewById(R.id.view_finder)
+        previewView = findViewById<PreviewView>(R.id.previewView)
 
         if (allPermissionsGranted()) {
-            viewFinder.post { startCamera() }
+            startCamera()
         } else {
             ActivityCompat.requestPermissions(
                 this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
@@ -41,96 +37,26 @@ class MainActivity : AppCompatActivity() {
     private val executor = Executors.newSingleThreadExecutor()
 
     //後に利用するTextureViewをここで定義します。
-    private lateinit var viewFinder: TextureView
+    private lateinit var previewView: PreviewView
 
     private fun startCamera() {
-        val previewConfig = PreviewConfig.Builder().apply {
-            setTargetResolution(Size(640, 480))
-        }.build()
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+        cameraProviderFuture.addListener(Runnable {
+            val cameraProvider = cameraProviderFuture.get()
+            var preview : Preview = Preview.Builder()
+                .setTargetResolution(Size(640, 480))
+                .build()
+            var cameraSelector : CameraSelector = CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                .build()
 
-        val preview = Preview(previewConfig)
+            preview.setSurfaceProvider(previewView.surfaceProvider)
 
-        preview.setOnPreviewOutputUpdateListener {
+            var camera = cameraProvider.bindToLifecycle(this as LifecycleOwner, cameraSelector, preview)
 
-            val parent = viewFinder.parent as ViewGroup
-
-            parent.removeView(viewFinder)
-            parent.addView(viewFinder, 0)
-
-            viewFinder.setSurfaceTexture(it.surfaceTexture)
-            updateTransform()
-        }
-        val imageCaptureConfig = ImageCaptureConfig.Builder()
-            .apply {
-                setCaptureMode(ImageCapture.CaptureMode.MIN_LATENCY)
-            }.build()
-
-        val imageCapture = ImageCapture(imageCaptureConfig)
-
-        findViewById<ImageButton>(R.id.capture_button).setOnClickListener {
-            val file = File(externalMediaDirs.first(),
-                "${System.currentTimeMillis()}.jpg")
-
-            imageCapture.takePicture(file,
-                object : ImageCapture.OnImageSavedListener {
-
-                    override fun onError(
-                        imageCaptureError: ImageCapture.UseCaseError,
-                        message: String,
-                        exc: Throwable?
-                    ) {
-                        val msg = "Photo capture failed: $message"
-                        Log.e("CameraXApp", msg, exc)
-                        viewFinder.post {
-                            Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
-                        }
-                    }
-
-                    override fun onImageSaved(file: File) {
-                        val msg = "Photo capture succeeded: ${file.absolutePath}"
-                        Log.d("CameraXApp", msg)
-                        viewFinder.post {
-                            Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
-                        }
-                    }
-                })
-        }
-
-        CameraX.bindToLifecycle(this, preview, imageCapture)
+        }, ContextCompat.getMainExecutor(this))
     }
 
-    private fun updateTransform() {
-        val matrix = Matrix()
-
-        val centerX = viewFinder.width / 2f
-        val centerY = viewFinder.height / 2f
-
-        val rotationDegrees = when(viewFinder.display.rotation) {
-            Surface.ROTATION_0 -> 0
-            Surface.ROTATION_90 -> 90
-            Surface.ROTATION_180 -> 180
-            Surface.ROTATION_270 -> 270
-            else -> return
-        }
-
-        matrix.postRotate(-rotationDegrees.toFloat(), centerX, centerY)
-
-        viewFinder.setTransform(matrix)
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
-        if (requestCode == REQUEST_CODE_PERMISSIONS) {
-            if (allPermissionsGranted()) {
-                viewFinder.post { startCamera() }
-            } else {
-                Toast.makeText(this,
-                    "Permissions not granted by the user.",
-                    Toast.LENGTH_SHORT).show()
-                finish()
-            }
-        }
-    }
     //マニフェストで指定されたすべての権限が付与されているかどうかを確認します
     private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
         ContextCompat.checkSelfPermission(
